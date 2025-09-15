10822683d81028f1fd498c3b84037233bbb3c627
sohail khan <sohailkhan.3095@gmail.com>
2025-09-15 16:07:30 +0530

    js update

--- bmi/script.js
+++ bmi/script.js
@@ -7,14 +7,20 @@ form.addEventListener("submit", (e) => {
   const weight = parseInt(document.querySelector("#weight").value);
   const bmi = weight / (height * height);
   const result = document.querySelector("#result");
-  
+
   if (height === "" || height < 0 || isNaN(height)) {
     result.innerHTML = `please give a valid height ${height}`;
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
     result.innerHTML = `please give a valid weight ${weight}`;
   } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    const bmi = (weight / ((height * height) / 10000)).toFixed(1);
     // show result
-    result.innerHTML = `  <span>Hi ${userName} Your BMI is ${bmi}</span>`;
+    if (bmi <= 18.6) {
+      result.innerHTML = `  <span>Hi ${userName} Your BMI is ${bmi} and you are under weight</span>`;
+    } else if (bmi <= 24.9) {
+      result.innerHTML = `  <span>Hi ${userName} Your BMI is ${bmi} and you are normal weight</span>`;
+    } else if (bmi > 24.9) {
+      result.innerHTML = `  <span>Hi ${userName} Your BMI is ${bmi} and you are Overweight</span>`;
+    }
   }
 });
